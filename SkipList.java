62734803b9759b5cde52393d30f69979e66e30a3
Oz <the.whizzard.of.oz@protonmail.com>
2020-03-27 12:38:43 -0700

    Intial Project Upload

---
+++ SkipList.java
@@ -0,0 +1,77 @@
+package SKPLIST_A4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SkipList implements SkipList_Interface {
+  private SkipList_Node root;
+  private final Random rand;
+  private double probability;
+  private const int MAXHEIGHT = 30; // the most links that a data cell may contain
+
+  public SkipList(int maxHeight) {
+    root = new SkipList_Node(Double.NaN, maxHeight);
+    rand = new Random();
+    probability = 0.5;
+  }
+
+  @Override
+  public void setSeed(long seed) { rand.setSeed(seed); }
+
+  @Override
+  public void setProbability(double probability) {
+     this.probability = probability;
+  }
+
+  private boolean flip() {
+    // use this where you "roll the dice"
+    // call it repeatedly until you determine the level
+    // for a new node
+    return rand.nextDouble() < probability;
+  }
+
+  @Override
+  public SkipList_Node getRoot() { return root; }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+
+    int levels;
+    for(levels = 0; levels < root.getNext().length && root.getNext(levels) != null; levels ++);
+
+    StringBuilder[] sbs = new StringBuilder[levels];
+
+    for(int i = 0; i < sbs.length; i ++) {
+      sbs[i] = new StringBuilder();
+      sbs[i].append("level ").append(i).append(":");
+    }
+
+    SkipList_Node cur = root;
+
+    while (cur.getNext(0) != null) {
+      cur = cur.getNext(0);
+      for(int i = levels - 1; i >= cur.getNext().length; i --) {
+        sbs[i].append("\t");
+      }
+      for(int i = cur.getNext().length - 1; i >= 0; i --) {
+        if (cur.getNext(i) == null) {
+          levels --;
+        }
+        sbs[i].append("\t").append(cur.getValue());
+      }
+    }
+
+    for(int i = sbs.length - 1; i >= 0; i --) {
+      sb.append(sbs[i]).append("\n");
+    }
+
+    return sb.toString();
+  }
+
+  //---------------------------------------------------------
+  // student code follows
+  // implement the methods of the interface
+  //---------------------------------------------------------
+
+}
